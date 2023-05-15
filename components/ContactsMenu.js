67ce6fc81f1f63301a10ebe94eb95ff3ccfd9678
Carlos Diaz <carlos12one@hotmail.com>
2023-05-15 13:47:00 -0500

    add home page

---
+++ components/ContactsMenu.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export const ContactsMenu = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <View></View>
+        <Text style={styles.text}>Starred</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
